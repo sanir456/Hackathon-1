8735c0025dd41526ad8697a074667ed4543b0fa5
Gopal1999 <goyalgopal359@gmail.com>
2021-11-14 19:45:16 +0530

    final update

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask,flash,render_template,url_for,request,session,g,redirect
+from flask import Flask,flash,render_template,url_for,request,session,g,redirect,session
 from flaskext.mysql import MySQL
 import os
+import random
+from util import nearby
 
 app = Flask(__name__)
 app.secret_key=os.urandom(24)
@@ -24,21 +26,14 @@ def index():
 #def loginfun():
 #    return render_template('/home/gopal/Desktop/hack_flip/ML001_MOVIE_RECOMMENDATION_ENGINE/templates/login.html')    
 
-@app.route('/successauth')
-def successauth():
-    return "Login Successful!"
 
-@app.route('/failedauth')
-def failedauth():
-    return "Either Email-id or password is incorrect!"    
+@app.route('/home', methods = ['POST','GET'])
+def home():
+    if 'user' in session:
+        restName,total,rating = nearby()
+        return render_template('home.html',name = session['name'],restName = restName,total=total,row = int(total/5)+1,rating = rating, dic = ["static/image/r1.jpg","static/image/r2.jpg","static/image/r3.jpg","static/image/r4.jpg","static/image/r5.jpg",])
 
-def fetchresname():
-    cursor.execute('show tables')
-    results = cursor.fetchall()
-    return results
 
-@app.route('/loginuser', methods = ['POST'])
-def loginuser():
     email_id = request.form['userEmailAdd']
     password = request.form['password']
     
@@ -47,9 +42,40 @@ def loginuser():
 
     for row in results:
         if email_id==row[1] and password==row[2]:
-            return render_template('home.html',name = row[0],restName = fetchresname(), dic = ["static/image/r1.jpg","static/image/r2.jpg","static/image/r3.jpg","static/image/r4.jpg","static/image/r5.jpg",])
+            restName,total,rating = nearby()
+            session['user'] = email_id
+            session['name'] = row[0]
+            print(row[0])
+            return render_template('home.html',name = row[0],restName = restName,total=total,row = int(total/5)+1,rating = rating, dic = ["static/image/r1.jpg","static/image/r2.jpg","static/image/r3.jpg","static/image/r4.jpg","static/image/r5.jpg",])
 
     return  render_template('login.html',perror = "Either Email-id or password is incorrect!")
+
+@app.route('/bestseller')
+def bestseller():
+    restName,total,rating = nearby()
+    name = session['name']
+    index = random.sample(range(total),5)
+    rn = []
+    rat = []
+    for r in index:
+        rn.append(restName[r])
+        rat.append(rating[r])
+    return render_template('home.html',name = name,restName = rn,total=5,row = 1,rating = rat, dic = ["static/image/r1.jpg","static/image/r2.jpg","static/image/r3.jpg","static/image/r4.jpg","static/image/r5.jpg",])
+
+
+
+@app.route('/myfav')
+def myfav():
+    restName,total,rating = nearby()
+    name = session['name']
+    index = random.sample(range(total),5)
+    rn = []
+    rat = []
+    for r in index:
+        rn.append(restName[r])
+        rat.append(rating[r])
+    return render_template('home.html',name = name,restName = rn,total=5,row = 1,rating = rat, dic = ["static/image/r1.jpg","static/image/r2.jpg","static/image/r3.jpg","static/image/r4.jpg","static/image/r5.jpg",])
+
 
 @app.route('/restaurant')
 def restaurant():
@@ -67,9 +93,6 @@ def login():
 
 
 
-@app.route('/success<fname><lname>')
-def success(fname, lname):
-    return 'Welcome '+fname + ' ' + lname
 
 @app.route('/registeruser', methods = ['POST'])
 def registeruser():
@@ -84,7 +107,11 @@ def registeruser():
     cursor.execute(query,value)
 
     conn.commit() 
-    return redirect(url_for('success',fname = first_name, lname = last_name))
+    restName,total,rating = nearby()
+    session['user'] = email_id
+    session['name'] = first_name
+            
+    return render_template('home.html',name = first_name,restName = restName,total=total,row = int(total/5)+1,rating = rating, dic = ["static/image/r1.jpg","static/image/r2.jpg","static/image/r3.jpg","static/image/r4.jpg","static/image/r5.jpg",])
 
 
 @app.route('/register')
@@ -95,15 +122,27 @@ def register():
 @app.route('/menu',methods=['POST'])
 def menu():
     name = request.form['restName']
-    cursor.execute("Select * from " + name)
+    cursor.execute("Select * from food_item")
     results = cursor.fetchall()
-    for row in results:
-        print(row)
-    #return render_template('menu.html')
+    index = random.sample(range(23),6)
+    item = []
+    for r in index:
+        item.append([results[r][1],results[r][2]])
+    
+    return render_template('menu.html',name=session['name'],item = item)
 
 @app.route('/cart')
 def cart():
     return render_template('cart.html')
+
+@app.route('/logout')
+def logout():
+    if 'user' in session:
+        session.clear()
+    return render_template('index.html')
+
+
+
 
 # if __name__ == '__main__':
 app.run(debug=True)
